e04920ebdcc09268c7eab27818e954f8e0fb5661
Ryan Tanaka <ryanyt@hawaii.edu>
2017-03-16 09:01:55 -1000

    updated

--- runSQL.java
+++ runSQL.java
@@ -78,7 +78,7 @@ public class runSQL
 					temp.setUsername(rs.getString(3));
 					temp.setPassword(rs.getString(4));
 					temp.setNodeNum(Integer.parseInt(rs.getString(5)));
-					t1PartitionMethod = rs.getString(6); // <- little repetetive
+					t1PartitionMethod = rs.getString(6); // <- little repetitive
 
 					table1_nodes_list.add(temp);
 				}
@@ -97,16 +97,126 @@ public class runSQL
 					temp.setUsername(rs.getString(3));
 					temp.setPassword(rs.getString(4));
 					temp.setNodeNum(Integer.parseInt(rs.getString(5)));
-					t1PartitionMethod = rs.getString(6); // <- little repetetive
+					t2PartitionMethod = rs.getString(6); // <- little repetitive
 
 					table2_nodes_list.add(temp);
 				}
 
+				//DBO
 				for(node n: table1_nodes_list)
 					n.printNode();
 
 				for(node n: table2_nodes_list)
 					n.printNode();
+
+				System.out.println("partition methods : " + t1PartitionMethod + " and " + t2PartitionMethod);
+
+				Connection connTest = DriverManager.getConnection(table1_nodes_list.get(0).getHostname(),
+												  				  table1_nodes_list.get(0).getUsername(),
+												   				  table1_nodes_list.get(0).getPassword());
+
+				Connection conn2Test = DriverManager.getConnection(table2_nodes_list.get(0).getHostname(),
+												   				   table2_nodes_list.get(0).getUsername(),
+												   				   table2_nodes_list.get(0).getPassword());
+
+				PreparedStatement psTest = null;
+				PreparedStatement ps2Test = null;
+				ResultSet rsTest = null;
+				ResultSet rs2Test = null;
+
+				String create = "";
+				final String ORDERS = "orders";
+				 String SHOW = "SHOW CREATE TABLE " + ORDERS;
+				psTest = connTest.prepareStatement(SHOW);
+				rsTest = psTest.executeQuery();
+
+				if(rsTest.next())
+				{
+					create = rsTest.getString(2);
+				}
+
+				create = create.replaceAll("CREATE", "CREATE TEMPORARY");
+				create = create.replaceAll("orders", "ordersTEMP");
+
+				System.out.println("create temp: " + create);
+
+				ps2Test = conn2Test.prepareStatement(create);
+				ps2Test.executeUpdate();
+
+				final String LINEITEM = "lineitem";
+				Statement stmt2 = conn2Test.createStatement();
+
+				psTest = connTest.prepareStatement("SELECT * FROM orders");
+				rsTest = psTest.executeQuery();
+
+				LinkedList<String> colNames = getColumnNames(table1_nodes_list.get(0), "orders");
+
+				int numCols = colNames.size();
+				LinkedList<String> dataTypes = getDataTypes(table1_nodes_list.get(0), "orders", numCols);
+
+				String temp = "";
+				String values = "";
+
+				for(int i = 0; i < numCols; i++)
+				{
+					values += colNames.get(i);
+					if(i < numCols - 1)
+					{
+						values += ", ";
+					}
+				}
+
+				while(rsTest.next())
+				{
+					for(int i = 1; i <= numCols; i++)
+					{
+						String dtype = dataTypes.get(i - 1);
+
+						if(dtype.equalsIgnoreCase("var") || dtype.equalsIgnoreCase("varchar") || 
+							dtype.equalsIgnoreCase("date") || dtype.equalsIgnoreCase("char"))
+						{
+							temp += "'";
+						}
+						temp += rsTest.getString(i);
+						
+						if(dtype.equalsIgnoreCase("var") || dtype.equalsIgnoreCase("varchar") || 
+							dtype.equalsIgnoreCase("date") || dtype.equalsIgnoreCase("char"))
+						{
+							temp += "'";
+						}
+
+						if(i <= numCols - 1)
+						{
+							temp+= ", ";
+						}					
+					}
+					System.out.println(temp);
+					System.out.println(values);
+					String insert = "INSERT INTO ordersTEMP (" + values + ") Values(" + temp + ")";
+					System.out.println(insert);
+					stmt2.executeUpdate(insert);
+					temp = "";
+				}
+
+				ps2Test = conn2Test.prepareStatement("SELECT * FROM ordersTEMP");
+				rs2Test = ps2Test.executeQuery();
+
+				System.out.println("SELECTING FROM TEMP TABLE");
+				String temp1 = "";
+				while(rs2Test.next())
+				{
+					for(int i = 1; i <= numCols; i++)
+					{
+						temp1 += rs2Test.getString(i);
+						if(i <= numCols - 1)
+						{
+							temp1+= ", ";
+						}
+					}
+
+					System.out.println(temp1);
+					temp1 = "";
+				}
 
 			}
 			catch(SQLException sqle)
@@ -176,6 +286,108 @@ public class runSQL
 	        //e.printStackTrace(System.err);
 	        e = e.getNextException();
 	    }
+	}
+
+	/**
+	* Gathers all column names of the table in the nodes we are targeting.
+	*
+	* @param targetNode the node we are targeting to retrieve column names
+	* @param tableName
+	* @return LinkedList<String> with column names
+	*/
+	public static LinkedList<String> getColumnNames(node targetNode, String tableName) throws SQLException
+	{
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		LinkedList<String> column_names = new LinkedList<String>();
+
+		try
+		{
+			conn = DriverManager.getConnection(	targetNode.getHostname(),
+												targetNode.getUsername(),
+												targetNode.getPassword());
+
+			ps = conn.prepareStatement("SELECT DISTINCT column_name FROM information_schema.columns WHERE table_name=?");
+			ps.setString(1, tableName);
+			rs = ps.executeQuery();
+
+			while(rs.next())
+				column_names.add(rs.getString(1));
+
+			if(ps != null)
+				ps.close();
+			if(rs != null)
+				rs.close();
+			if(conn != null)
+				conn.close();
+
+			//DEBUG
+			System.out.println("DEBUG OUTPUT: column_names from [" + targetNode.getHostname() + "]");
+
+			for(String s : column_names)
+				System.out.print(s + " ");
+
+			System.out.println("");
+
+			return column_names;
+		}
+		catch(SQLException sqle)
+		{
+			System.out.println("Error: unable to retreive column names from [" + targetNode.getHostname() + "]");
+			throw sqle;
+		}
+	}
+	
+	/**
+	* Gathers all data types of the table in the nodes we are targeting.
+	*
+	* @param targetNode the node we are targeting to retrieve data types
+	* @param tableName
+	* @return LinkedList<String> with data types
+	*/
+	public static LinkedList<String> getDataTypes(node targetNode, String tableName, int numCols) throws SQLException
+	{
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		LinkedList<String> data_types = new LinkedList<String>();
+
+		try
+		{
+			conn = DriverManager.getConnection(	targetNode.getHostname(),
+												targetNode.getUsername(),
+												targetNode.getPassword());
+
+			ps = conn.prepareStatement("SELECT data_type FROM information_schema.columns WHERE table_name=?");
+			ps.setString(1, tableName);
+			rs = ps.executeQuery();
+
+			for(int i = 0; rs.next() && i < numCols; i ++)
+				data_types.add(rs.getString(1));
+
+			if(ps != null)
+				ps.close();
+			if(rs != null)
+				rs.close();
+			if(conn != null)
+				conn.close();
+
+			//DEBUG
+			System.out.println("DEBUG OUTPUT: data_types from [" + targetNode.getHostname() + "]");
+
+			for(String s : data_types)
+				System.out.print(s + " ");
+
+			System.out.println("");
+
+			return data_types;
+		}
+		catch(SQLException sqle)
+		{
+			System.out.println("Error: unable to retreive data types from [" + targetNode.getHostname() + "]");
+			throw sqle;
+		}
 	}
 }
 
