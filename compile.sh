efab3883a893bf96a75b7cfedee21966fb202d16
Ryan Tanaka <ryanyt@hawaii.edu>
2017-03-20 22:57:21 -1000

    updated

--- compile.sh
+++ compile.sh
@@ -2,5 +2,6 @@
 
 jdbclib="./mysql-connector-java-5.1.40-bin.jar"
 antlrlib="./antlr-4.6-complete.jar"
+parserlib="./commons-csv-1.4.jar"
 
-javac -cp $jdbclib:$antlrlib:. *.java
+javac -cp $jdbclib:$antlrlib:$parserlib:. *.java
--- run.sh
+++ run.sh
@@ -2,5 +2,9 @@
 
 jdbclib="./mysql-connector-java-5.1.40-bin.jar"
 antlrlib="./antlr-4.6-complete.jar"
+parserlib="./commons-csv-1.4.jar"
 
-java -cp $jdbclib:$antlrlib:. runSQL clustercfg sqlfile_single
+#1 : cluster config
+#2 : ddl, sql query, or csv file
+
+java -cp $jdbclib:$antlrlib:$parserlib:. runSQL $1 $2 
